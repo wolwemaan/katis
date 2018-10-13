0acca3dbc742aa7525bda1105da6f2a8761732a7
ev <you@example.com>
2018-10-12 21:35:05 -0400

    ada

--- src/Islands.java
+++ src/Islands.java
@@ -1,78 +1,131 @@
 import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class Islands {
 
-	static int memnum = 0;
+	final static char land = 'L';
+	final static char water = 'W';
+	final static char cloud = 'C';
+
+	private static Boolean isItThat(char[][] arr, int i, int j, int row, int col, char checkFor) {
+		if (i < 0 || j < 0 || i >= row || j >= col)
+			return null;
+
+		if (arr[i][j] != checkFor)
+			return Boolean.FALSE;
+
+		return Boolean.TRUE;
+	}
 
 	public static void main(String[] args) {
+		
+		// Get Input
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
 		int col = sc.nextInt();
 		sc.nextLine();
 		char[][] arr = new char[row][col];
-		for (int i = 0; i < row; i++) {
-			arr[i] = sc.nextLine().toCharArray();
-		}
+		for (int i = 0; i < row; i++) arr[i] = sc.nextLine().toCharArray();
 
 		sc.close();
 
-		final char land = 'L';
-		final char water = 'W';
-		final char cloud = 'C';
+		// Get rid of clouds and make optimistic guess to get maximum amount of land
+		BiFunction<Integer, Integer, Boolean> lamdaA = (i, j) -> {
+			checkForAndSetPositive(arr, row, col, i, j, land, land);
+			checkForAndSetNegative(arr, row, col, i, j, water, water);
+			return Boolean.TRUE;
+		};
 
-		if (hor(arr, row, col, cloud) == (row * col) || hor(arr, row, col, cloud) == (row * col)) {
-			System.out.println("0");
-			return;
-		}
+		BiFunction<Integer, Integer, Boolean> lamdaB = (i, j) -> {
+			checkForAndSetPositive(arr, row, col, i, j, cloud, water);
+			checkForAndSetPositive(arr, row, col, i, j, water, water);
+			return Boolean.TRUE;
+		};
 
-		int hct = hor(arr, row, col, land, cloud);
-		int vct = ver(arr, row, col, land, cloud);
+		exhaustIterationsUntilNothingChangesAnymore(lamdaA, row, col, arr);
+		exhaustIterationsUntilNothingChangesAnymore(lamdaB, row, col, arr);
 
-		System.out.println(Math.abs(hct - vct));
+		// At this point arr is prepared and should not contain not any clouds at all
+		// TODO Comment print out and add the star count code here
+		System.out.print(getGridAsString(row, col, arr));
 
 	}
 
-	private static int ver(char[][] arr, int row, int col, char c, char c2) {
-		int res = 0;
-		boolean unint = false;
-		for (int j = 0; j < col; j++) {
+	private static void exhaustIterationsUntilNothingChangesAnymore(BiFunction<Integer, Integer, Boolean> lamda,
+			int row, int col, char[][] arr) {
+		String old = "";
+		while (haveClouds(row, col, arr)) {
 			for (int i = 0; i < row; i++) {
-				if (arr[i][j] == c || arr[i][j] == c2 ) {
-					if (!unint) {
-						res++;
-						unint = true;
-					}
-				} else unint = false;
+				for (int j = 0; j < col; j++)
+					lamda.apply(i, j);
+
 			}
+			String now = getGridAsString(row, col, arr);
+			if (now.compareTo(old) == 0)
+				break;
+			old = now;
 		}
-		return res;
+
 	}
 
-	private static int hor(char[][] arr, int row, int col, char c, char c2) {
-		int res = 0;
-		boolean unint = false;
+	private static void checkForAndSetPositive(char[][] arr, int row, int col, int i, int j, char checkFor,
+			char setAs) {
+		// System.out.println(":"+i+':'+j);
+		if (arr[i][j] == cloud) {
+			Boolean top = isItThat(arr, i - 1, j, row, col, checkFor);
+			Boolean left = isItThat(arr, i, j - 1, row, col, checkFor);
+			Boolean right = isItThat(arr, i, j + 1, row, col, checkFor);
+			Boolean bottom = isItThat(arr, i + 1, j, row, col, checkFor);
+
+			if ((top != null && top.booleanValue()) || (left != null && left.booleanValue())
+					|| (right != null && right.booleanValue()) || (bottom != null && bottom.booleanValue()))
+				arr[i][j] = setAs;
+			//System.out.println("cloud:" + i + ':' + j + top + left + right + bottom + arr[i][j]);
+
+		}
+
+	}
+
+	private static void checkForAndSetNegative(char[][] arr, int row, int col, int i, int j, char checkFor,
+			char setAs) {
+		if (arr[i][j] == cloud) {
+			Boolean top = isItThat(arr, i - 1, j, row, col, checkFor);
+			Boolean left = isItThat(arr, i, j - 1, row, col, checkFor);
+			Boolean right = isItThat(arr, i, j + 1, row, col, checkFor);
+			Boolean bottom = isItThat(arr, i + 1, j, row, col, checkFor);
+
+			if ((top == null || top.booleanValue()) && (left == null || left.booleanValue())
+					&& (right == null || right.booleanValue()) && (bottom == null || bottom.booleanValue()))
+				arr[i][j] = setAs;
+			// System.out.println("cloud:"+i+':'+j+top+left+right+bottom + arr[i][j]);
+
+		}
+
+	}
+
+	private static String getGridAsString(int row, int col, char[][] arr) {
+
+		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				if (arr[i][j] == c || arr[i][j] == c2) {
-					if (!unint) {
-						res++;
-						unint = true;
-					}
-				} else unint = false;
+				sb.append(arr[i][j]);
 			}
+			sb.append('\n');
 		}
-		return res;
+		return sb.toString();
+
 	}
-	
-	private static int hor(char[][] arr, int row, int col, char c) {
-		int res = 0;
+
+	private static boolean haveClouds(int row, int col, char[][] arr) {
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				if (arr[i][j] == c)
-					res++;
+				if (arr[i][j] == cloud)
+					return true;
 			}
 		}
-		return res;
+		return false;
 	}
+
+
 
 }
--- test/IslandsTest.java
+++ test/IslandsTest.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 
 
@@ -34,6 +38,73 @@ public class IslandsTest {
 			errContent = new ByteArrayOutputStream();
 			System.setErr(new PrintStream(errContent));
 		}
+		
+		
+		@Test
+		void test5() throws IOException {
+
+			String[] args = null;
+			
+			// [to change!] Specific filename base to testcase 
+			final String filenameBase = "Islands-0005";
+			
+			InputStream fips = kattiPrepare(filenameBase);
+
+			// [to change!] Change class name
+			Islands.main(args);
+
+			kattiAssert(fips, filenameBase);
+		}
+		
+		@Test
+		void test6() throws IOException {
+
+			String[] args = null;
+			
+			// [to change!] Specific filename base to testcase 
+			final String filenameBase = "Islands-0006";
+			
+			InputStream fips = kattiPrepare(filenameBase);
+
+			// [to change!] Change class name
+			Islands.main(args);
+
+			kattiAssert(fips, filenameBase);
+		}
+		
+		
+		
+		@Test
+		void test3() throws IOException {
+
+			String[] args = null;
+			
+			// [to change!] Specific filename base to testcase 
+			final String filenameBase = "Islands-0003";
+			
+			InputStream fips = kattiPrepare(filenameBase);
+
+			// [to change!] Change class name
+			Islands.main(args);
+
+			kattiAssert(fips, filenameBase);
+		}
+		
+		@Test
+		void test4() throws IOException {
+
+			String[] args = null;
+			
+			// [to change!] Specific filename base to testcase 
+			final String filenameBase = "Islands-0004";
+			
+			InputStream fips = kattiPrepare(filenameBase);
+
+			// [to change!] Change class name
+			Islands.main(args);
+
+			kattiAssert(fips, filenameBase);
+		}
 
 		@AfterEach
 		public void restoreStreams() {
@@ -49,6 +120,22 @@ public class IslandsTest {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		
+		@Test
+		void test0() throws IOException {
+
+			String[] args = null;
+			
+			// [to change!] Specific filename base to testcase 
+			final String filenameBase = "Islands-0000";
+			
+			InputStream fips = kattiPrepare(filenameBase);
+
+			// [to change!] Change class name
+			Islands.main(args);
+
+			kattiAssert(fips, filenameBase);
 		}
 
 		@Test
@@ -162,6 +249,98 @@ public class IslandsTest {
 //
 //			kattiAssert(fips, filenameBase);
 //		}
+		// if (!top && !left && !right && !bottom) arr[i][j] = water;
+		// if (hor(arr, row, col, cloud) == (row * col) || hor(arr, row, col, cloud) ==
+		// (row * col)) {
+		// System.out.println("0");
+		// return;
+		// }
+
+		// Set<Pair> landmap =
+		// hor2(arr, row, col, land);
+		//
+		// Set<Pair> cloudmap =
+		// hor2(arr, row, col, cloud);
+		//
+		// Iterator <Pair> iter = landmap.iterator();
+		// while (iter.hasNext()) {
+		// Pair pair = iter.next();
+		// }
+		// int vct = ver(arr, row, col, land, cloud);
+
+		// System.out.println(Math.abs(hct - vct));
+
+		// }
+
+		// private static int ver(char[][] arr, int row, int col, char c, char c2) {
+		// int res = 0;
+		// boolean unint = false;
+		// for (int j = 0; j < col; j++) {
+		// for (int i = 0; i < row; i++) {
+		// if (arr[i][j] == c || arr[i][j] == c2 ) {
+		// if (!unint) {
+		// res++;
+		// unint = true;
+		// }
+		// } else unint = false;
+		// }
+		// }
+		// return res;
+		// }
+
+		private static Set<Pair> hor2(char[][] arr, int row, int col, char c) {
+			Set<Pair> map = new LinkedHashSet<Pair>();
+
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					if (arr[i][j] == c) {
+						map.add(new Pair(i, j));
+					}
+				}
+			}
+			return map;
+		}
+
+		private static int hor(char[][] arr, int row, int col, char c) {
+			int res = 0;
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					if (arr[i][j] == c)
+						res++;
+				}
+			}
+			return res;
+		}
+		
+
+
+		static class Pair {
+			int key;
+			int value;
+
+			public Pair(int key, int value) {
+				super();
+				this.key = key;
+				this.value = value;
+			}
+
+			public int getKey() {
+				return key;
+			}
+
+			public void setKey(int key) {
+				this.key = key;
+			}
+
+			public int getValue() {
+				return value;
+			}
+
+			public void setValue(int value) {
+				this.value = value;
+			}
+
+		}
 		
 		private void kattiAssert(InputStream fips, String filenameBase) throws IOException {
 			fips.close();
